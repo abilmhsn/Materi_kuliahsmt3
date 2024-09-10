5e4c9d07bc71af69bcb0a010e40d0b8bbfa9f919
abilmhsn <muhsinlabibul@gmail.com>
2024-09-10 11:59:08 +0700

    create TBL

---
+++ DATABASE/INSERT TBL.sql
@@ -0,0 +1,12 @@
+INSERT INTO Mahasiswa (nama, nim, jurusan, angkatan)
+VALUES
+('Lavonte','123456894','Teknik Informatika', 2022),
+('Kimberly','1234568831','Sistem Informasi', 2021),
+('Kojak','123456892','Teknik komputer', 2020),
+('Abil','223456891','Teknik Informatika', 2019),
+('Juni','123456890','Teknik Informatika', 2018),
+('Joni','123456891','Teknik Informatika', 2017),
+('Yuani','123453892','Teknik Informatika', 2016),
+('Yuana','123456893','Teknik Informatika', 2015),
+('Boy','1234568010','Teknik Informatika', 2014),
+('Agus','123456809','Teknik Informatika', 2012)
