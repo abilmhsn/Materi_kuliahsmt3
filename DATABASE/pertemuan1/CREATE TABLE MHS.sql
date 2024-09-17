635bacd8b474608950c4bf2924adfbf55e8240a6
ITEKes Mahardika <abilmhsn>
2024-09-17 11:49:15 +0700

    pertemuan2

--- DATABASE/pertemuan1/CREATE TABLE MHS.sql
+++ DATABASE/pertemuan1/CREATE TABLE MHS.sql
@@ -1,7 +1,7 @@
 /*membuat tabel matakuliah*/
 CREATE TABLE mahasiswa(
 id_mahasiswa INT PRIMARY KEY auto_increment,
-nama VARCHAR(100),
+nama VARCHAR(100),performance_schemasys
 nim VARCHAR(15) UNIQUE,
 jurusan VARCHAR(50),
 ankatan YEAR )
